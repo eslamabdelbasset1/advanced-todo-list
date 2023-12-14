df569088f91506c3d811d323d14cf6e2e25fe274
eslamabdelbasset1 <eslamabdelbasset1@gmail.com>
2023-12-14 17:18:07 +0200

    Create index blade and database

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ToDO List</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css ">
+
+</head>
+<body class="bg-gray-200 p-4">
+    @yield('content')
+</body>
+</html>
